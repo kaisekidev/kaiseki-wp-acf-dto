466cf64dcf78f55d10ba10b782b3d9f4de987a38
David Mondok <3883758+davidmondok@users.noreply.github.com>
2023-05-30 12:31:01 +0200

    Add mapper

---
+++ src/Mappers/SnakeAlphanumericCaseMapper.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kaiseki\WordPress\ACF\Dto\Mappers;
+
+use Illuminate\Support\Str;
+use Spatie\LaravelData\Mappers\NameMapper;
+
+use function is_string;
+
+class SnakeAlphanumericCaseMapper implements NameMapper
+{
+    /**
+     * The cache of snake-cased words.
+     *
+     * @var array<string,string>
+     */
+    protected static array $cache = [];
+
+    public function map(int|string $name): string|int
+    {
+        if (!is_string($name)) {
+            return $name;
+        }
+
+        return self::convert(Str::snake($name));
+    }
+
+    /**
+     * Convert a string to snake case.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    private static function convert(string $value): string
+    {
+        $key = $value;
+
+        if (isset(static::$cache[$key])) {
+            return static::$cache[$key];
+        }
+
+        if ((bool)\Safe\preg_match('~[0-9]+~', $value)) {
+            $value = \Safe\preg_replace('/(?<=[a-zA-Z])(\d)/u', '_$1', $value);
+        }
+
+        return static::$cache[$key] = $value;
+    }
+}
