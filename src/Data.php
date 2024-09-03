995eb1db1c9b411a19f95ad75f804c3e3417cf94
David Mondok <3883758+davidmondok@users.noreply.github.com>
2024-09-03 15:49:36 +0200

    fix missing implementation of method

--- src/Data.php
+++ src/Data.php
@@ -6,6 +6,7 @@ namespace Kaiseki\WordPress\ACF\Dto;
 
 use Kaiseki\WordPress\ACF\Dto\Concerns\WithSafeFrom;
 use Spatie\LaravelData\Concerns\BaseData;
+use Spatie\LaravelData\Concerns\ContextableData;
 use Spatie\LaravelData\Concerns\TransformableData;
 use Spatie\LaravelData\Contracts\BaseData as BaseDataContract;
 use Spatie\LaravelData\Contracts\TransformableData as TransformableDataContract;
@@ -15,4 +16,5 @@ class Data implements BaseDataContract, TransformableDataContract
     use TransformableData;
     use BaseData;
     use WithSafeFrom;
+    use ContextableData;
 }
