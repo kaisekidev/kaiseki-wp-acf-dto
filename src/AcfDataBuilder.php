6d9af6fdc3a60896c6ae5bcf1866cd4d25a623c7
David Mondok <david@woda.at>
2024-12-03 22:54:59 +0100

    feat: add defaults arg to data-builder

--- src/AcfDataBuilder.php
+++ src/AcfDataBuilder.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Kaiseki\WordPress\ACF\Dto;
 
+use function array_merge;
+
 final class AcfDataBuilder
 {
     public function __construct(protected readonly AcfGetFields $acfGetFields)
@@ -13,18 +15,23 @@ final class AcfDataBuilder
     /**
      * @template T
      *
-     * @param class-string<T> $className
-     * @param mixed           $postId
-     * @param ?array<string>  $excludeFromFormatting
+     * @param class-string<T>      $className
+     * @param mixed                $postId
+     * @param array<string, mixed> $defaults
+     * @param ?array<string>       $excludeFromFormatting
      *
      * @return T
      */
     public function create(
         string $className,
         mixed $postId = false,
+        array $defaults = [],
         ?array $excludeFromFormatting = null,
     ) {
-        $fieldData = $this->acfGetFields->getFields($postId, $excludeFromFormatting) ?? [];
+        $fieldData = array_merge(
+            $defaults,
+            $this->acfGetFields->getFields($postId, $excludeFromFormatting) ?? [],
+        );
 
         return $className::from($fieldData);
     }
